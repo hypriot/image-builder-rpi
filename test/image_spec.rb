99d467ae55bee64babe2943ae03eb96f73ee4e64
Govinda Fichtner <govinda.fichtner@googlemail.com>
2016-01-13 21:18:41 +0100

    fix fstab specs and moving them into image_spec
    Maybe later we will split tests up into different files.
    But for now gonna keep them together.

--- test/image_spec.rb
+++ test/image_spec.rb
@@ -59,4 +59,16 @@ describe "SD-Card Image" do
       expect(stdout).to contain('arm')
     end
   end
+
+  context "/etc/fstab" do
+    let(:stdout) { command("guestfish add #{image_path} : run : mount /dev/sda2 / : cat /etc/fstab").stdout }
+
+    it "has a vfat boot entry" do
+      expect(stdout).to contain('/dev/mmcblk0p1 /boot vfat')
+    end
+
+    it "has a ext4 root entry" do
+      expect(stdout).to contain('/dev/mmcblk0p2 / ext4')
+    end
+  end
 end
