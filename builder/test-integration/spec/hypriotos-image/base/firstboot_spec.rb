7cd5ced71b2a3381064f9e54253813540cf3d69c
Stefan Scherer <scherer_stefan@icloud.com>
2016-02-12 02:46:52 +0100

    Check rootfs version, check firstboot conitional file

---
+++ builder/test-integration/spec/hypriotos-image/base/firstboot_spec.rb
@@ -0,0 +1,3 @@
+describe file('/etc/hypriot-firstboot_not_to_be_run') do
+  it { should be_file }
+end
--- builder/test-integration/spec/hypriotos-image/base/release_spec.rb
+++ builder/test-integration/spec/hypriotos-image/base/release_spec.rb
@@ -6,5 +6,5 @@ describe file('/etc/os-release') do
   it { should be_file }
   it { should be_owned_by 'root' }
   its(:content) { should match /HYPRIOT_OS="HypriotOS\/armhf"/ }
-  its(:content) { should match /HYPRIOT_TAG="v0.6.1"/ }
+  its(:content) { should match /HYPRIOT_TAG="v0.7.0"/ }
 end
