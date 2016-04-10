e4c2643e6077ab00ceaf0284f19cd1c70cc81483
Govinda Fichtner <govindaf@Govindas-MBP.fritz.box>
2016-04-10 13:57:50 +0200

    fix bluetooth rspec tests

--- builder/test-integration/spec/hypriotos-image/base/rpi3_bluetooth.rb
+++
@@ -1,9 +0,0 @@
-# only test for built-in bluetooth support if we are on the Raspberry Pi 3
-cpu_info = `cat /proc/cpu_info`
-
-if cpu_info.include?('a02082') or cpu_info.include?('a22082')
-  describe command('hciconfig') do
-    its(:stdout) { should contain /hci0/ }
-    its(:stdout) { should contain /UP RUNNING/ }
-  end
-end
