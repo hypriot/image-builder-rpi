db653bc51e7aff29e69bb72197714b9b73ee1d43
Roland Huß <roland@ro14nd.de>
2016-04-25 13:27:30 +0200

    Fixed cgroup-enable with cgroup_enable
    Accroding to http://slurm.schedmd.com/cgroups.html and other sources, '_' is the proper separator.

--- builder/test-integration/spec/hypriotos-image/cmdline_kernel_spec.rb
+++ builder/test-integration/spec/hypriotos-image/cmdline_kernel_spec.rb
@@ -5,7 +5,7 @@ describe file('/boot/cmdline.txt') do
   its(:content) { should match /\+dwc_otg.lpm_enable=0/ }
   its(:content) { should match /root=\/dev\/mmcblk0p2/ }
   its(:content) { should match /rootfstype=ext4/ }
-  its(:content) { should match /cgroup-enable=memory/ }
+  its(:content) { should match /cgroup_enable=memory/ }
   its(:content) { should match /swapaccount=1/ }
   its(:content) { should match /elevator=deadline/ }
   its(:content) { should match /fsck.repair=yes/ }
