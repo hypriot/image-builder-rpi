d22c24b517552a0827f760725e1e74db1e962569
Govinda Fichtner <govindaf@Govindas-MBP.fritz.box>
2016-05-06 15:26:48 +0200

    update Docker Swarm to 1.2.1

--- builder/test-integration/spec/hypriotos-docker/docker-swarm_spec.rb
+++ builder/test-integration/spec/hypriotos-docker/docker-swarm_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-describe file('/var/local/rpi-swarm_v1.2.0.tar.gz') do
+describe file('/var/local/rpi-swarm_v1.2.1.tar.gz') do
   it { should be_file }
 end
 
 describe command('docker run --rm -t hypriot/rpi-swarm --version') do
-  its(:stdout) { should match /swarm version 1.2.0 \(HEAD\)/ }
+  its(:stdout) { should match /swarm version 1.2.1 \(HEAD\)/ }
   its(:exit_status) { should eq 0 }
 end
 
 describe command('docker images hypriot/rpi-swarm') do
-  its(:stdout) { should match /hypriot\/rpi-swarm .*latest .*fa7ce19d5211 / }
+  its(:stdout) { should match /hypriot\/rpi-swarm .*latest .*8cd4dc8bb230 / }
   its(:exit_status) { should eq 0 }
 end
