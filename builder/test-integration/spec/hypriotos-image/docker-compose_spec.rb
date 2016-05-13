1fb7451989e8c280f695b4d732af16df71759dfd
Dieter Reuter <dieter.reuter@me.com>
2016-05-14 01:51:41 +0200

    Update Docker Compose to 1.7.0

--- builder/test-integration/spec/hypriotos-image/docker-compose_spec.rb
+++ builder/test-integration/spec/hypriotos-image/docker-compose_spec.rb
@@ -6,7 +6,7 @@ end
 
 describe command('dpkg -l docker-compose') do
   its(:stdout) { should match /ii  docker-compose/ }
-  its(:stdout) { should match /1.6.2-28/ }
+  its(:stdout) { should match /1.7.0-30/ }
   its(:exit_status) { should eq 0 }
 end
 
@@ -17,6 +17,6 @@ describe file('/usr/local/bin/docker-compose') do
 end
 
 describe command('docker-compose --version') do
-  its(:stdout) { should match /1.6.2/m }
+  its(:stdout) { should match /1.7.0/m }
   its(:exit_status) { should eq 0 }
 end
