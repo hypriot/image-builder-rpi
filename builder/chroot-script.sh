91106724303da64488c6950a758b692adb8eeb08
Stefan Scherer <scherer_stefan@icloud.com>
2016-07-31 09:50:47 +0200

    Revert "Set ip_conntrack_tcp_be_liberal to improve network stability"

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -207,7 +207,3 @@ systemctl disable cluster-lab.service
 echo "HYPRIOT_DEVICE=\"$HYPRIOT_DEVICE\"" >> /etc/os-release
 echo "HYPRIOT_IMAGE_VERSION=\"$HYPRIOT_IMAGE_VERSION\"" >> /etc/os-release
 cp /etc/os-release /boot/os-release
-
-# improve network stability, hypriot/image-builder-rpi#57
-cat etc/sysctl.conf
-echo "net.ipv4.netfilter.ip_conntrack_tcp_be_liberal = 1" >> etc/sysctl.conf
