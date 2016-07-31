478684f9581a42dc4b6590ff4b996a4038f93853
Dieter Reuter <dieter.reuter@me.com>
2016-07-31 10:53:57 +0200

    Force overriding the tar.gz
    This is to avoid an interactive question if you're build the image locally in a Vagrantbox. With the flag `-f` gzip will just override the tar.gz without manual interuption.
    
    Signed-off-by: Dieter Reuter <dieter.reuter@me.com>

--- builder/get-cluster-lab-images.sh
+++ builder/get-cluster-lab-images.sh
@@ -5,9 +5,9 @@ source versions.config || source ../versions.config
 # pull save and zip consul
 docker pull hypriot/rpi-consul:"${CONSUL_VERSION}"
 docker save --output=builder/files/var/local/rpi-consul_"${CONSUL_VERSION}".tar hypriot/rpi-consul:"${CONSUL_VERSION}"
-gzip builder/files/var/local/rpi-consul_"${CONSUL_VERSION}".tar
+gzip -f builder/files/var/local/rpi-consul_"${CONSUL_VERSION}".tar
 
 # pull save and zip swarm
 docker pull hypriot/rpi-swarm:"${SWARM_VERSION}"
 docker save --output=builder/files/var/local/rpi-swarm_"${SWARM_VERSION}".tar hypriot/rpi-swarm:"${SWARM_VERSION}"
-gzip builder/files/var/local/rpi-swarm_"${SWARM_VERSION}".tar
+gzip -f builder/files/var/local/rpi-swarm_"${SWARM_VERSION}".tar
