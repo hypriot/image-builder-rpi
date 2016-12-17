ca1a6f72d343bce6bf8d143ee62b04cc4a696532
Dieter Reuter <dieter.reuter@me.com>
2016-12-17 14:25:54 +0100

    Update to official Docker Engine 1.12.5
    Signed-off-by: Dieter Reuter <dieter.reuter@me.com>

--- builder/test-integration/spec/hypriotos-image/docker_spec.rb
+++ builder/test-integration/spec/hypriotos-image/docker_spec.rb
@@ -6,7 +6,7 @@ end
 
 describe command('dpkg -l docker-engine') do
   its(:stdout) { should match /ii  docker-engine/ }
-  its(:stdout) { should match /1.12.3-0~jessie/ }
+  its(:stdout) { should match /1.12.5-0~deb/ }
   its(:exit_status) { should eq 0 }
 end
 
@@ -86,13 +86,13 @@ describe file('/etc/bash_completion.d/docker') do
 end
 
 describe command('docker -v') do
-  its(:stdout) { should match /Docker version 1.12.3, build/ }
+  its(:stdout) { should match /Docker version 1.12.5, build/ }
   its(:exit_status) { should eq 0 }
 end
 
 describe command('docker version') do
-  its(:stdout) { should match /Client:. Version:      1.12.3. API version:  1.24/m }
-  its(:stdout) { should match /Server:. Version:      1.12.3. API version:  1.24/m }
+  its(:stdout) { should match /Client:. Version:      1.12.5. API version:  1.24/m }
+  its(:stdout) { should match /Server:. Version:      1.12.5. API version:  1.24/m }
   its(:exit_status) { should eq 0 }
 end
 
