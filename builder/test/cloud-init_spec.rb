d63d4bd6e5e143ef0ffc549ee180cb2da9c1c975
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-24 18:44:37 +0100

    Test user-data symlink

--- builder/test/cloud-init_spec.rb
+++ builder/test/cloud-init_spec.rb
@@ -8,11 +8,10 @@ describe file('/usr/lib/cloud/scripts/per-once/resizefs') do
   it { should be_file }		
 end
 
-describe file('/boot/user-data') do
-  it { should be_file }
-  its(:content) { should match /hostname: / }
+describe file('/var/lib/cloud/seed/nocloud-net/user-data') do
+  it { should be_linked_to '/boot/user-data' }
 end
 
-describe file('/boot/meta-data') do
-  it { should be_file }
+describe file('/var/lib/cloud/seed/nocloud-net/meta-data') do
+  it { should be_linked_to '/boot/meta-data' }
 end
