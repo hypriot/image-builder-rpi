7c223595809a8738372bf9343d8b2af09226791d
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-24 19:45:50 +0100

    Add cloud-init tests

--- builder/test-integration/spec/hypriotos-image/cloud-init_spec.rb
+++ builder/test-integration/spec/hypriotos-image/cloud-init_spec.rb
@@ -12,3 +12,19 @@ end
 describe file('/boot/meta-data') do
   it { should be_file }
 end
+
+describe file('/var/lib/cloud/scripts/per-once/regenerate-machine-id') do
+  it { should be_file }
+end
+
+describe file('/var/lib/cloud/scripts/per-once/resizefs') do
+  it { should be_file }
+end
+
+describe file('/var/lib/cloud/seed/nocloud-net/user-data') do
+  it { should be_linked_to '/boot/user-data' }
+end
+
+describe file('/var/lib/cloud/seed/nocloud-net/meta-data') do
+  it { should be_linked_to '/boot/meta-data' }
+end
