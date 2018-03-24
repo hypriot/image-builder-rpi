808aa25ece464fd387f395a76dbaafbab0e09ceb
Stefan Scherer <scherer_stefan@icloud.com>
2018-03-24 17:46:55 +0100

    Add tests for cloud-init

---
+++ builder/test/cloud-init_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'		
+
+describe file('/usr/lib/cloud/scripts/per-once/regenerate-machine-id') do		
+  it { should be_file }		
+end
+
+describe file('/usr/lib/cloud/scripts/per-once/resizefs') do		
+  it { should be_file }		
+end
+
+describe file('/boot/user-data') do
+  it { should be_file }
+  its(:content) { should match /hostname: / }
+end
+
+describe file('/boot/meta-data') do
+  it { should be_file }
+end
--- builder/test/os-release_spec.rb
+++ builder/test/os-release_spec.rb
@@ -36,8 +36,8 @@ describe "Root filesystem" do
     expect(stdout).to contain('^HYPRIOT_DEVICE="Raspberry Pi"$')
   end
 
-  it "uses os-rootfs version 'HYPRIOT_OS_VERSION=\"v1.2.6\"'" do
-    expect(stdout).to contain('^HYPRIOT_OS_VERSION="v1.2.6"$')
+  it "uses os-rootfs version 'HYPRIOT_OS_VERSION=\"v2.0.0\"'" do
+    expect(stdout).to contain('^HYPRIOT_OS_VERSION="v2.0.0"$')
   end
 
   if ENV.fetch('TRAVIS_TAG','') != ''
