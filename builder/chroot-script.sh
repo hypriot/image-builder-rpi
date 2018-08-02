bef32ddadcd40224bb1d378dd52d568c5a73de01
Sebastian Silva <sebastian@sugarlabs.org>
2018-08-02 11:31:12 -0500

    Enable camera (#9)
    Enable camera in boot parameters

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -174,9 +174,9 @@ enable_uart=0
 " > boot/config.txt
 
 echo "# camera settings, see http://elinux.org/RPiconfig#Camera
-start_x=0
+start_x=1
 disable_camera_led=1
-gpu_mem=16
+gpu_mem=128
 " >> boot/config.txt
 
 # /etc/modules
