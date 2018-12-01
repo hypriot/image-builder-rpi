f2dfa23834a5081537f6ac3ad1bf3741462dac26
Stefan Scherer <scherer_stefan@icloud.com>
2018-12-01 16:02:48 +0100

    Update Docker 18.09.0

--- builder/test-integration/spec/hypriotos-image/base/kernel_spec.rb
+++ builder/test-integration/spec/hypriotos-image/base/kernel_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe command('uname -r') do
-  its(:stdout) { should match /4.14.70(-v7)?+/ }
+  its(:stdout) { should match /4.14.79(-v7)?+/ }
   its(:exit_status) { should eq 0 }
 end
 
-describe file('/lib/modules/4.14.70-hypriotos+/kernel') do
+describe file('/lib/modules/4.14.79-hypriotos+/kernel') do
   it { should be_directory }
 end
 
-describe file('/lib/modules/4.14.70-hypriotos-v7+/kernel') do
+describe file('/lib/modules/4.14.79-hypriotos-v7+/kernel') do
   it { should be_directory }
 end
