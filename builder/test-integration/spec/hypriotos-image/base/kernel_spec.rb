4ab48f49d8f37f13b5d069b61a65072072a3cda2
Stefan Scherer <scherer_stefan@icloud.com>
2019-02-24 11:33:19 +0100

    Update Docker 18.06.3, update kernel 4.14.98

--- builder/test-integration/spec/hypriotos-image/base/kernel_spec.rb
+++ builder/test-integration/spec/hypriotos-image/base/kernel_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe command('uname -r') do
-  its(:stdout) { should match /4.14.79(-v7)?+/ }
+  its(:stdout) { should match /4.14.98(-v7)?+/ }
   its(:exit_status) { should eq 0 }
 end
 
-describe file('/lib/modules/4.14.79+/kernel') do
+describe file('/lib/modules/4.14.98+/kernel') do
   it { should be_directory }
 end
 
-describe file('/lib/modules/4.14.79-v7+/kernel') do
+describe file('/lib/modules/4.14.98-v7+/kernel') do
   it { should be_directory }
 end
