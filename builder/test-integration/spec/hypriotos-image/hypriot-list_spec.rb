f1ab58343c8d9f7823f358abaa48a74b74e90dde
Stefan Scherer <scherer_stefan@icloud.com>
2019-02-24 11:26:11 +0100

    Remove packagecloud, upstream kernel is used

--- builder/test-integration/spec/hypriotos-image/hypriot-list_spec.rb
+++ builder/test-integration/spec/hypriotos-image/hypriot-list_spec.rb
@@ -1,10 +1,7 @@
 require 'spec_helper'
 
 describe file('/etc/apt/sources.list.d/hypriot.list') do
-  it { should be_file }
-  it { should be_mode 644 }
-  it { should be_owned_by 'root' }
-  it { should contain 'deb https://packagecloud.io/Hypriot/rpi/debian/ stretch main' }
+  it { should_not be_file }
 end
 
 describe package('apt-transport-https') do
