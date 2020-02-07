f6ed39d4a046f092b228227d1188640f27b98e18
Stefan Scherer <stefan.scherer@docker.com>
2020-02-07 12:12:29 +0100

    Update tests

--- builder/test-integration/spec/hypriotos-image/base/kernel_spec.rb
+++ builder/test-integration/spec/hypriotos-image/base/kernel_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe command('uname -r') do
-  its(:stdout) { should match /4.19.75(-v7)?+/ }
+  its(:stdout) { should match /4.19.97(-v7)?+/ }
   its(:exit_status) { should eq 0 }
 end
 
-describe file('/lib/modules/4.19.75+/kernel') do
+describe file('/lib/modules/4.19.97+/kernel') do
   it { should be_directory }
 end
 
-describe file('/lib/modules/4.19.75-v7+/kernel') do
+describe file('/lib/modules/4.19.97-v7+/kernel') do
   it { should be_directory }
 end
